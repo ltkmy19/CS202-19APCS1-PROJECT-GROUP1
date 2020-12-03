8be5e7675d161ecf07332d5198775de8605333cd
LeThiKimMy <63316690+ltkmy19@users.noreply.github.com>
2020-12-03 17:08:53 +0700

    fix menu

--- Road_Crossing/Road_Crossing/main.cpp
+++ Road_Crossing/Road_Crossing/main.cpp
@@ -13,7 +13,7 @@ int main() {
 	mciSendString("play WeWillRockYou.mp3 repeat", NULL, 0, NULL);
 	int character = 0;//male
 
-	string Menu[4] = { "--New Game--", "--Load Game--", "--Setting--", "--Quit--" };
+	string Menu[6] = { "--New Game--", "--Load Game--", "--Sound--", "--Character--","--Tutorial--", "--Quit--" };
 	int pointer = 0;
 	int line = 2;
 	while (true)
@@ -24,13 +24,17 @@ int main() {
 
 		string Art = getFileContents(Reader);     
 
-		cout << Art << endl;              
+		cout << Art << endl;     
+		Green();
+		gotoXY(70, 1);
 
+		cout << "MENU" << endl;
 		Reader.close();
 		White();
-		for (int i = 0; i < 4; ++i)
+		for (int i = 0; i < 6; ++i)
 		{
-			gotoXY(80, 3 + i + line);
+			gotoXY(65, 3 + i);
+
 			if (i == pointer)
 			{
 				Green();
@@ -38,9 +42,11 @@ int main() {
 			}
 			else
 			{
+
 				White();
 				cout << Menu[i] << endl;
 			}
+			
 		}
 		White();
 		while (true)
@@ -50,14 +56,14 @@ int main() {
 				pointer -= 1;
 				if (pointer == -1)
 				{
-					pointer = 2;
+					pointer = 5;
 				}
 				break;
 			}
 			else if (GetAsyncKeyState(VK_DOWN) != 0)
 			{
 				pointer += 1;
-				if (pointer == 4)
+				if (pointer == 6)
 				{
 					pointer = 0;
 				}
@@ -65,31 +71,63 @@ int main() {
 			}
 			else if (GetAsyncKeyState(VK_RETURN) != 0)
 			{
+				system("cls");
+
 				switch (pointer)
 				{
 				case 0:
 				{
-					system("cls");
 					system("pause >nul");
 					break;
 				}
 				case 1:
 				{
-					system("cls");
 					system("pause >nul");
 
 					break;
 				}
 				case 2:
 				{
+					LightMagenta();
+					ifstream Reader("TextGraphic.txt");
 
+					string Art = getFileContents(Reader);
+
+					cout << Art << endl;
+					
 					break;
 				}
 				case 3:
 				{
+					LightMagenta();
+					ifstream Reader("TextGraphic.txt");
+
+					string Art = getFileContents(Reader);
+
+					cout << Art << endl;
+					break;
+				}
+				case 4:
+				{
+					LightMagenta();
+					ifstream Reader("TextGraphic.txt");
+
+					string Art = getFileContents(Reader);
+
+					cout << Art << endl;
+					gotoXY(70, 5);
+
+					cout << "Using w,a,s,d keys to move up, left, down, right.";
+					cout << "There are 10 rounds for the game. Pass all of them to win a trophy.";
+					break;
+				}
+				case 5:
+				{
+					
 					return 0;
 					break;
 				}
+			
 				default:
 				{
 					break;
