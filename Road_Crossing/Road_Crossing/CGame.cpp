4290e84d4fc93373dace3e96bd85dd61bd7c6591
BuiQuangHuy19125047 <quanghuybui93@gmail.com>
2020-12-04 23:33:39 +0700

    Quang Huy 4/12

---
+++ Road_Crossing/Road_Crossing/CGame.cpp
@@ -0,0 +1,56 @@
+#include "CGame.h"
+using namespace std;
+
+//CGame::CGAME(){
+//
+//}
+
+void CGame::drawGame()
+CGame::~CGAME(){
+    delete[] axh;
+    delete[] axt;
+    delete[] akl;
+    delete[] ac;
+    delete[] lv;
+}
+
+CPEOPLE CGame::getPeople(){
+    return cn;
+}
+
+CVEHICLE* CGame::getVehicle(){
+    int c = /*max car at this level*/;
+    int t = /*max truck at this level*/;
+    CVEHICLE* tmp = new CVEHICLE[c+t];
+    for(int i = 0;i < c;i++){
+        tmp[i] = axh[i];
+    }
+    for(int i = c;i < t;i++){
+        tmp[i] = axt[i];
+    }
+    return tmp;
+}
+
+CANIMAL* CGame::getAnimal(){
+    int d = /*max dinasour at this level*/;
+    int b = /*max bird at this level*/;
+    CVEHICLE* tmp = new CVEHICLE[c+t];
+    for(int i = 0;i < d;i++){
+        tmp[i] = akl[i];
+    }
+    for(int i = d;i < b;i++){
+        tmp[i] = ac[i];
+    }
+    return tmp;
+}
+
+//void CGame::resetGame()
+//void CGame::exitGame(HANDLE)
+//void CGame::startGame()
+//void CGame::loadGame(istream)
+//void CGame::saveGame(istream)
+//void CGame::pauseGame(HANDLE)
+//void CGame::resumeGame(HANDLE)
+//void CGame::updatePosPeople(char)
+//void CGame::updatePosVehicle()
+//void CGame::updatePosAnimal()
