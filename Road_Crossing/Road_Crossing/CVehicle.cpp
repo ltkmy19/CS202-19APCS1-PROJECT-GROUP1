a651587b53fa8bbc9cc0450d218b0e30f4bc0ed8
quangvust201 <quangvust201@gmail.com>
2020-12-15 16:05:25 +0700

    A wild CVehicle has appeared !!
    A. Fight
    B. Hide
    C. Run

--- Road_Crossing/Road_Crossing/CVehicle.cpp
+++ Road_Crossing/Road_Crossing/CVehicle.cpp
@@ -1,2 +1,42 @@
 #include "CVehicle.h"
 
+//CVEHICLE
+
+CVEHICLE::CVEHICLE(int tmpX, int tmpY)
+{
+	x = tmpX;
+	y = tmpY;
+}
+
+void CVEHICLE::Move(int tmpX, int tmpY)
+{
+	x = tmpX;
+	y = tmpY;
+}
+
+bool CVEHICLE::IsDone()
+{
+	return true;
+}
+
+//CTRUCK
+CTRUCK::CTRUCK(int tmpX, int tmpY) : CVEHICLE::CVEHICLE(tmpX,tmpY)
+{
+
+}
+
+void CTRUCK::Draw(int tmpX, int tmpY)
+{
+
+}
+
+//CCAR
+CCAR::CCAR(int tmpX, int tmpY) : CVEHICLE::CVEHICLE(tmpX, tmpY)
+{
+
+}
+
+void CCAR::Draw(int tmpX, int tmpY)
+{
+
+}
--- Road_Crossing/Road_Crossing/CVehicle.h
+++ Road_Crossing/Road_Crossing/CVehicle.h
@@ -1,14 +1,30 @@
-#ifndef _CVEHICLE_H_
-#define _CVEHICLE_H_
-
 #include "Console.h"
-#include "Files.h"
 
-CVEHICLE(int x, int y);
-void virtual Draw(int x, int y) = 0;
-void Move(int, int);
-bool IsDone();
-int getX() { return mX; }
-int getY() { return mY; }
+class CVEHICLE
+{
+private:
+	int x, y;
+public:
+	CVEHICLE(int tmpX, int tmpY);
+	void virtual Draw(int tmpX, int tmpY) = 0;
+	void Move(int, int);
+	bool IsDone();
+	int getX() { return x; }
+	int getY() { return y; }
+};
 
-#endif
+class CTRUCK: public CVEHICLE
+{
+private:
+public:
+	CTRUCK(int x, int y);
+	void Draw(int x, int y);
+};
+
+class CCAR : public CVEHICLE
+{
+private:
+public:
+	CCAR(int x, int y);
+	void Draw(int x, int y);
+};
