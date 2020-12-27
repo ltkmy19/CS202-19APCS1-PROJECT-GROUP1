ab53dc79403ca26dd65e21d83ec05e77a9133f9a
quangvust201 <quangvust201@gmail.com>
2020-12-27 20:37:50 +0700

    CAnimal Update
    Add more functions

--- Road_Crossing/Road_Crossing/CAnimal.cpp
+++ Road_Crossing/Road_Crossing/CAnimal.cpp
@@ -1,2 +1,38 @@
 #include "CAnimal.h"
 
+CANIMAL::CANIMAL(int x, int y) 
+{
+
+}
+
+void CANIMAL::Move(int x, int y)
+{
+
+}
+
+bool CANIMAL::IsDone()
+{
+
+}
+
+CDINAUSOR::CDINAUSOR(int x, int y) : CANIMAL(x, y) 
+{
+
+}
+
+void CDINAUSOR::Draw(int x, int y)
+{
+
+}
+
+CBIRD::CBIRD(int x, int y) : CANIMAL(x, y) 
+{
+
+}
+
+void CBIRD::Draw(int x, int y) 
+{
+
+
+}
+
--- Road_Crossing/Road_Crossing/CAnimal.h
+++ Road_Crossing/Road_Crossing/CAnimal.h
@@ -1,2 +1,26 @@
 #include "Console.h"
 
+class CANIMAL {
+protected:
+	int mX, mY, Way;
+public:
+	CANIMAL(int x, int y);
+	void virtual Draw(int x, int y) = 0;
+	void Move(int, int);
+	bool IsDone();
+	int getX() { return mX; }
+	int getY() { return mY; }
+};
+
+class CDINAUSOR : public CANIMAL {
+public:
+	CDINAUSOR(int x, int y);
+	void Draw(int x, int y);
+};
+
+class CBIRD : public CANIMAL {
+public:
+	CBIRD(int x, int y);
+	void Draw(int x, int y);
+};
+
