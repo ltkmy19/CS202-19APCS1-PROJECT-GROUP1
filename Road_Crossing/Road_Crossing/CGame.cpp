16406508e4a98fcb630f4b90aa1697127944168f
BuiQuangHuy19125047 <quanghuybui93@gmail.com>
2021-01-01 23:47:10 +0700

    add function CGame::IsCrashed() and fix some variables

--- Road_Crossing/Road_Crossing/CGame.cpp
+++ Road_Crossing/Road_Crossing/CGame.cpp
@@ -1,177 +1,186 @@
-//#include "CGame.h"
-//#include <fstream>
-//using namespace std;
-//
-//CGame::CGAME(){
-//
-//}
-//
-//void CGAME::drawGame() {
-//    LightMagenta();
-//    gotoXY(0, 0); cout << (char)218; for (int i = 1; i < WIDTH; i++) { cout << (char)196; } cout << (char)191;
-//    gotoXY(0, 1); for (int i = 2; i < HEIGHT; i++) { cout << (char)179; gotoXY(0, i); }
-//    gotoXY(0, HEIGHT - 1); cout << (char)192; for (int i = 1; i < WIDTH; i++) { cout << (char)196; }
-//    gotoXY(WIDTH, 1); for (int i = 2; i < HEIGHT; i++) { cout << (char)179; gotoXY(WIDTH, i); } cout << (char)217;
-//
-//    int W = WIDTH + 8;
-//    int H = HEIGHT - 10;
-//    gotoXY(W, 2); cout << (char)218; for (int i = W+1; i < W+30;i++) { cout << (char)196; } cout << (char)191;
-//    gotoXY(W, 3); for (int i = 4; i < H; i++) { cout << (char)179; gotoXY(W, i); }
-//    gotoXY(W, H-1); cout << (char)192; for (int i = W+1; i < W+30; i++) { cout << (char)196; }
-//    gotoXY(W+30, 3); for (int i = 3; i < H; i++) { cout << (char)179; gotoXY(W+30, i); } cout << (char)217;
-//
-//    LightCyan();
-//    gotoXY(W + 1, 4); cout << "WELCOME TO CROSSING ROAD! ^_^";
-//    gotoXY(W + 7, 6); cout << "press ESC to exit ";
-//    gotoXY(W + 5, 8); cout << "Level: 1";
-//    gotoXY(W + 5, 10); cout << "P: Pause/Resume ";
-//    gotoXY(W + 6, 12); cout << "Please pause before: ";
-//    gotoXY(W + 5, 14); cout << "K: Load game ";
-//    gotoXY(W + 5, 16); cout << "S: Save game ";
-//}
-//
-//CGAME::~CGAME(){
-//    delete[] axh;
-//    delete[] axt;
-//    delete[] akl;
-//    delete[] ac;
-//    delete[] lv;
-//}
-//
-//CPEOPLE CGAME::getPeople(){
-//    return cn;
-//}
-//
-//vector <CVEHICLE*> CGAME::getVehicle(){
-//    int c = /*max car at this level*/;
-//    int t = /*max truck at this level*/;
-//    vector<CVEHICLE*>tmp;
-//    for(int i = 0;i < c;i++){
-//        tmp.push_back(&(axh[i]));
-//    }
-//    for(int j = 0;j < t;j++){
-//        tmp.push_back(&(axt[j]));
-//    }
-//    return tmp;
-//}
-//
-//vector <CANIMAL*> CGAME::getAnimal(){
-//    int d = /*max dinasour at this level*/;
-//    int b = /*max bird at this level*/;
-//    vector<CANIMAL*>tmp;
-//    for(int i = 0;i < d;i++){
-//        tmp.push_back(&(akl[i]));
-//    }
-//    for(int j = 0;j < b;j++){
-//        tmp.push_back(&(ac[j]));
-//    }
-//    return tmp;
-//}
-//
-//void CGAME::resetGame()
-//void CGAME::exitGame(HANDLE)
-//void CGAME::startGame()
-//
-//
-//void CGAME::loadGame(){
-//    ifstream fin;
-//    int numberOfSave = 0;
-//    int level= 0, line = 0;
-//    string name = "";
-//    FileSave *tmp = nullptr;
-//    fin.open("SaveFiles.txt");
-//    if(!fin.is_open()) cout << "Can not open file!" << endl;
-//    else{
-//        if(fin.eof()) cout << "There is no save file!"<<endl;
-//        else{
-//            while(!fin.eof()){
-//                fin >> level;
-//                getline(fin,name);
-//                tmp = new FileSave(level, name);
-//                this->File.push_back(tmp);
-//                gotoXY(WIDTH+8,line);
-//                cout << numberOfSave+1 <<". "<< File[numberOfSave]->getName() <<" (Level: "<<File[numberOfSave]->getLevel()<<")";
-//                numberOfSave++;
-//                line++;
-//
-//            }
-//            char type = _getch();
-//            if(type != 27){
-//                int a = type - '0';
-//                if(a > 0 && a <= numberOfSave){
-//                    this->curLevel = File[a]->getLevel()-1;
-//                    updateLevel();
-//                }
-//            }
-//        }
-//        fin.close();
-//    }
-//}
-//
-//void CGAME::saveGame(){
-//	Clean();
-//	ofstream fout;
-//	string name = "";
-//	char a = 'a';
-//	int W = WIDTH + 6;
-//	int H = HEIGHT - 5;
-//	int cnt = 0;
-//	gotoXY(W, H); cout << "Input the name of your file" << endl;
-//	gotoXY(W, H + 1); cout << "(maximum 15 characters) : " << endl;
-//	while (a != 27) {
-//		gotoXY(W + 2, H + 2); cout << name << "                    ";
-//		a = _getch();
-//		if (a == 8 && name.size() > 0) {
-//			name.pop_back();
-//			cnt--;
-//		}
-//		else if (a == 13) {
-//			fout.open("SaveFile.txt");
-//			if (!fout.is_open()) {
-//				GotoXY(W + 2, H + 4); cout << "Can not open file!";
-//				Sleep(500);
-//				return;
-//			}
-//
-//			fout << name << endl;
-//			fout << presentLevel << endl;
-//			fout.close();
-//			GotoXY(W + 2, H + 4); cout << "Save successfully!";
-//			Sleep(2000);
-//			break;
-//
-//		}
-//		else if (name.size() >= 15) {
-//			gotoXY(W , H + 4); cout << "Your string are too long";
-//			Sleep(500);
-//			name = "";
-//			cnt = 0;
-//			Clean();
-//            gotoXY(W, H); cout << "Input the name of your file" << endl;
-//            gotoXY(W, H + 1); cout << "(maximum 15 characters) : " << endl;
-//		}
-//		else {
-//			name += a;
-//			if (a != 8) cnt++;
-//		}
-//	}
-//	Clean();
-//}
-//void CGAME::pauseGame(HANDLE)
-//void CGAME::resumeGame(HANDLE)
-//void CGAME::updatePosPeople(char)
-//void CGAME::updatePosVehicle()
-//void CGAME::updatePosAnimal()
+#include "CGame.h"
+#include <fstream>
+using namespace std;
 
-//Level::Level(int lane, int distance, int speed, int car, int struck, int bird, int dinasour) {
-//	this->lane = lane; this->speed = speed; this->car = car; this->truck = struck;
-//	this->bird = bird; this->dina = dinasour; this->distance = distance;
-//}
-//
-//int Level::getLane() { return lane; }
-//int Level::getDistance() { return distance; }
-//int Level::getSpeed() { return speed; }
-//int Level::AmounrOfBird() { return bird; }
-//int Level::AmountOfCar() { return car; }
-//int Level::AmountOfDinasour() { return dina; }
-//int Level::AmountOfTruck() { return truck; }
+CGame::CGAME(){
+
+}
+
+void CGAME::drawGame() {
+    LightMagenta();
+    gotoXY(0, 0); cout << (char)218; for (int i = 1; i < WIDTH; i++) { cout << (char)196; } cout << (char)191;
+    gotoXY(0, 1); for (int i = 2; i < HEIGHT; i++) { cout << (char)179; gotoXY(0, i); }
+    gotoXY(0, HEIGHT - 1); cout << (char)192; for (int i = 1; i < WIDTH; i++) { cout << (char)196; }
+    gotoXY(WIDTH, 1); for (int i = 2; i < HEIGHT; i++) { cout << (char)179; gotoXY(WIDTH, i); } cout << (char)217;
+
+    int W = WIDTH + 8;
+    int H = HEIGHT - 10;
+    gotoXY(W, 2); cout << (char)218; for (int i = W+1; i < W+30;i++) { cout << (char)196; } cout << (char)191;
+    gotoXY(W, 3); for (int i = 4; i < H; i++) { cout << (char)179; gotoXY(W, i); }
+    gotoXY(W, H-1); cout << (char)192; for (int i = W+1; i < W+30; i++) { cout << (char)196; }
+    gotoXY(W+30, 3); for (int i = 3; i < H; i++) { cout << (char)179; gotoXY(W+30, i); } cout << (char)217;
+
+    LightCyan();
+    gotoXY(W + 1, 4); cout << "WELCOME TO CROSSING ROAD! ^_^";
+    gotoXY(W + 7, 6); cout << "press ESC to exit ";
+    gotoXY(W + 5, 8); cout << "Level: 1";
+    gotoXY(W + 5, 10); cout << "P: Pause/Resume ";
+    gotoXY(W + 6, 12); cout << "Please pause before: ";
+    gotoXY(W + 5, 14); cout << "K: Load game ";
+    gotoXY(W + 5, 16); cout << "S: Save game ";
+}
+
+CGAME::~CGAME(){
+    delete[] axh;
+    delete[] axt;
+    delete[] akl;
+    delete[] ac;
+    delete[] lv;
+}
+
+CPEOPLE CGAME::getPeople(){
+    return cn;
+}
+
+vector <CVEHICLE*> CGAME::getVehicle(){
+    int c = lv[curLevel]->AmountOfCar();
+    int t = lv[curLevel]->AmountOfTruck();;
+    vector<CVEHICLE*>tmp;
+    for(int i = 0;i < c;i++){
+        tmp.push_back(&(axh[i]));
+    }
+    for(int j = 0;j < t;j++){
+        tmp.push_back(&(axt[j]));
+    }
+    return tmp;
+}
+
+vector <CANIMAL*> CGAME::getAnimal(){
+    int d = lv[curLevel]->AmountOfDinasour();
+    int b = lv[curLevel]->AmountOfBird();
+    vector<CANIMAL*>tmp;
+    for(int i = 0;i < d;i++){
+        tmp.push_back(&(akl[i]));
+    }
+    for(int j = 0;j < b;j++){
+        tmp.push_back(&(ac[j]));
+    }
+    return tmp;
+}
+
+void CGAME::resetGame()
+void CGAME::exitGame(HANDLE)
+void CGAME::startGame()
+
+
+void CGAME::loadGame(){
+    ifstream fin;
+    int numberOfSave = 0;
+    int level= 0, line = 0;
+    string name = "";
+    FileSave *tmp = nullptr;
+    fin.open("SaveFiles.txt");
+    if(!fin.is_open()) cout << "Can not open file!" << endl;
+    else{
+        if(fin.eof()) cout << "There is no save file!"<<endl;
+        else{
+            while(!fin.eof()){
+                fin >> level;
+                getline(fin,name);
+                tmp = new FileSave(level, name);
+                this->File.push_back(tmp);
+                gotoXY(WIDTH+8,line);
+                cout << numberOfSave+1 <<". "<< File[numberOfSave]->getName() <<" (Level: "<<File[numberOfSave]->getLevel()<<")";
+                numberOfSave++;
+                line++;
+
+            }
+            char type = _getch();
+            if(type != 27){
+                int a = type - '0';
+                if(a > 0 && a <= numberOfSave){
+                    this->curLevel = File[a]->getLevel()-1;
+                    updateLevel();
+                }
+            }
+        }
+        fin.close();
+    }
+}
+
+void CGAME::saveGame(){
+	Clean();
+	ofstream fout;
+	string name = "";
+	char a = 'a';
+	int W = WIDTH + 6;
+	int H = HEIGHT - 5;
+	int cnt = 0;
+	gotoXY(W, H); cout << "Input the name of your file" << endl;
+	gotoXY(W, H + 1); cout << "(maximum 15 characters) : " << endl;
+	while (a != 27) {
+		gotoXY(W + 2, H + 2); cout << name << "                    ";
+		a = _getch();
+		if (a == 8 && name.size() > 0) {
+			name.pop_back();
+			cnt--;
+		}
+		else if (a == 13) {
+			fout.open("SaveFile.txt");
+			if (!fout.is_open()) {
+				GotoXY(W + 2, H + 4); cout << "Can not open file!";
+				Sleep(500);
+				return;
+			}
+
+			fout << name << endl;
+			fout << presentLevel << endl;
+			fout.close();
+			GotoXY(W + 2, H + 4); cout << "Save successfully!";
+			Sleep(2000);
+			break;
+
+		}
+		else if (name.size() >= 15) {
+			gotoXY(W , H + 4); cout << "Your string are too long";
+			Sleep(500);
+			name = "";
+			cnt = 0;
+			Clean();
+            gotoXY(W, H); cout << "Input the name of your file" << endl;
+            gotoXY(W, H + 1); cout << "(maximum 15 characters) : " << endl;
+		}
+		else {
+			name += a;
+			if (a != 8) cnt++;
+		}
+	}
+	Clean();
+}
+void CGAME::pauseGame(HANDLE){
+    //Pause thread;
+}
+
+void CGAME::resumeGame(HANDLE){
+    //return to thread;
+}
+
+bool CGAME::isCrashed(){
+    return (cn.isImpact() || cn.isImpact() || cn.isImpact() || cn.isImpact());
+}
+void CGAME::updatePosPeople(char)
+void CGAME::updatePosVehicle()
+void CGAME::updatePosAnimal()
+
+Level::Level(int lane, int distance, int speed, int car, int struck, int bird, int dinasour) {
+	this->lane = lane; this->speed = speed; this->car = car; this->truck = struck;
+	this->bird = bird; this->dina = dinasour; this->distance = distance;
+}
+
+int Level::getLane() { return lane; }
+int Level::getDistance() { return distance; }
+int Level::getSpeed() { return speed; }
+int Level::AmounrOfBird() { return bird; }
+int Level::AmountOfCar() { return car; }
+int Level::AmountOfDinasour() { return dina; }
+int Level::AmountOfTruck() { return truck; }
--- Road_Crossing/Road_Crossing/CGame.h
+++ Road_Crossing/Road_Crossing/CGame.h
@@ -1,85 +1,86 @@
 //#ifndef CGAME_H_INCLUDED
 //#define CGAME_H_INCLUDED
-//#include "Console.h"
-//#include "Files.h"
-//#include "Traffic_Light.h"
-//#include <stdio.h>
-//#include <vector>
-//using namespace std;
-//
-//int WIDTH = 80;
-//int HEIGHT = 29;
-//
-//class FileSave{
-//private:
-//    int level;
-//    string name;
-//public:
-//    FileSave() {};
-//    FileSave(int lv,string name){
-//        this->level = lv;
-//        this->name = name;
-//    }
-//    ~FileSave() {};
-//    int getLevel(){
-//        return level;
-//    }
-//    string getName(){
-//        return name;
-//    }
-//};
+#include "Console.h"
+#include "Files.h"
+#include "Traffic_Light.h"
+#include <stdio.h>
+#include <vector>
+using namespace std;
 
-//class Level {
-//private:
-//	int lane; // number of lanes
-//	int distance;
-//	int speed; // speed of obstacle
-//	int car, truck, dina, bird;
-//public:
-//	Level(int, int, int, int, int, int, int);
-//	int getLane();
-//	int getDistance();
-//	int getSpeed();
-//	int AmountOfCar();
-//	int AmountOfTruck();
-//	int AmountOfDinasour();
-//	int AmounrOfBird();
-//};
+int WIDTH = 80;
+int HEIGHT = 29;
+
+class FileSave{
+private:
+    int level;
+    string name;
+public:
+    FileSave() {};
+    FileSave(int lv,string name){
+        this->level = lv;
+        this->name = name;
+    }
+    ~FileSave() {};
+    int getLevel(){
+        return level;
+    }
+    string getName(){
+        return name;
+    }
+};
+
+class Level {
+private:
+	int lane; // number of lanes
+	int distance;
+	int speed; // speed of obstacle
+	int car, truck, dina, bird;
+public:
+	Level(int, int, int, int, int, int, int);
+	int getLane();
+	int getDistance();
+	int getSpeed();
+	int AmountOfCar();
+	int AmountOfTruck();
+	int AmountOfDinasour();
+	int AmounrOfBird();
+};
 
 
-//class CGAME{
-//private:
-//    CTRUCK* axt;
-//    CCAR* axh;
-//    CDINASOUR* akl;
-//    CBIRD* ac;
-//    CPEOPLE cn;
-//    LEVEL *lv;
-//	 int curLevel = 0;
-//    vector<FileSave*>File;
+class CGAME{
+private:
+    CTRUCK* axt;
+    CCAR* axh;
+    CDINASOUR* akl;
+    CBIRD* ac;
+    CPEOPLE cn;
+    LEVEL *lv;
+    int curLevel = 0;
+    vector<FileSave*>File;
 
 public:
-//    CGAME() {}; //Chuẩn bị dữ liệu cho tất cả các đối tượng
-//    void drawGame(); //Thực hiện vẽ trò chơi ra màn hình sau khi có dữ liệu
-//    ~CGAME() {}; // Hủy tài nguyên đã cấp phát
-//    CPEOPLE getPeople();//Lấy thông tin người
-//    vector <CVEHICLE*> getVehicle();//Lấy danh sách các xe
-//    vector <CANIMAL> getAnimal(); //Lấy danh sách các thú
-//    void resetGame(); // Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
-//    void exitGame(HANDLE); // Thực hiện thoát Thread
-//    void startGame(); // Thực hiện bắt đầu vào trò chơi
-//    void loadGame(); // Thực hiện tải lại trò chơi đã lưu
-//    void saveGame(); // Thực hiện lưu lại dữ liệu trò chơi
-//    void pauseGame(HANDLE); // Tạm dừng Thread
-//    void resumeGame(HANDLE); //Quay lai Thread
-//    void updatePosPeople(char); //Thực hiện điều khiển di chuyển của CPEOPLE
-//    void updatePosVehicle(); //Thực hiện cho CTRUCK & CCAR di chuyển
-//    void updatePosAnimal();//Thực hiện cho CDINAUSOR & CBIRD di chuyển
-//    void updateLevel();
-//    int getCurLevel() {
-//        return curLevel;
-//    }
-//    void Clean();
-//};
-//
+    CGAME() {}; //Chuẩn bị dữ liệu cho tất cả các đối tượng
+    void drawGame(); //Thực hiện vẽ trò chơi ra màn hình sau khi có dữ liệu
+    ~CGAME() {}; // Hủy tài nguyên đã cấp phát
+    CPEOPLE getPeople();//Lấy thông tin người
+    vector <CVEHICLE*> getVehicle();//Lấy danh sách các xe
+    vector <CANIMAL> getAnimal(); //Lấy danh sách các thú
+    void resetGame(); // Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
+    void exitGame(HANDLE); // Thực hiện thoát Thread
+    void startGame(); // Thực hiện bắt đầu vào trò chơi
+    void loadGame(); // Thực hiện tải lại trò chơi đã lưu
+    void saveGame(); // Thực hiện lưu lại dữ liệu trò chơi
+    void pauseGame(HANDLE); // Tạm dừng Thread
+    void resumeGame(HANDLE); //Quay lai Thread
+    void updatePosPeople(char); //Thực hiện điều khiển di chuyển của CPEOPLE
+    void updatePosVehicle(); //Thực hiện cho CTRUCK & CCAR di chuyển
+    void updatePosAnimal();//Thực hiện cho CDINAUSOR & CBIRD di chuyển
+    bool isCrashed();
+    void updateLevel();
+    int getCurLevel() {
+        return curLevel;
+    }
+    void Clean();
+};
+
 #endif // CGAME_H_INCLUDED
