3b25d7a0ebd5c8b8d7e7af3f243796f223546153
LeThiKimMy <63316690+ltkmy19@users.noreply.github.com>
2021-01-05 23:13:16 +0700

    TrafficLight
    Fixed

--- Road_Crossing/Road_Crossing/Traffic_Light.cpp
+++ Road_Crossing/Road_Crossing/Traffic_Light.cpp
@@ -1,40 +1,59 @@
 #include"Traffic_Light.h"
 
+//void Lights::RedLight(int x, int y) {
+//	gotoXY(x, y - 1);
+//	Red();
+//	cout << char(219);
+//	gotoXY(x, y);
+//	DarkGray();
+//	cout << char(219);
+//	gotoXY(x, y + 1);
+//	DarkGray();
+//	cout << char(219);
+//	White();
+//
+//}
+//void Lights::GreenLight(int x, int y) {
+//	gotoXY(x, y - 1);
+//	DarkGray();
+//	cout << char(219);
+//	gotoXY(x, y);
+//	DarkGray();
+//	cout << char(219);
+//	gotoXY(x, y + 1);
+//	Green();
+//	cout << char(219);
+//	White();
+//}
+//
+//void Lights::YellowLight(int x, int y) {
+//	gotoXY(x, y - 1);
+//	DarkGray();
+//	cout << char(219);
+//	gotoXY(x, y);
+//	Yellow();
+//	cout << char(219);
+//	gotoXY(x, y + 1);
+//	DarkGray();
+//	cout << char(219);
+//	White();
+//}
+
 void Lights::RedLight(int x, int y) {
 	gotoXY(x, y - 1);
 	Red();
-	cout << char(219);
+	cout << char(220); 
 	gotoXY(x, y);
 	DarkGray();
-	cout << char(219);
-	gotoXY(x, y + 1);
-	DarkGray();
-	cout << char(219);
+	cout << char(223);
 	White();
-
 }
 void Lights::GreenLight(int x, int y) {
 	gotoXY(x, y - 1);
 	DarkGray();
-	cout << char(219);
+	cout << char(220);
 	gotoXY(x, y);
-	DarkGray();
-	cout << char(219);
-	gotoXY(x, y + 1);
-	Green();
-	cout << char(219);
-	White();
-}
-
-void Lights::YellowLight(int x, int y) {
-	gotoXY(x, y - 1);
-	DarkGray();
-	cout << char(219);
-	gotoXY(x, y);
-	Yellow();
-	cout << char(219);
-	gotoXY(x, y + 1);
-	DarkGray();
-	cout << char(219);
+	LightGreen();
+	cout << char(223); 
 	White();
 }
